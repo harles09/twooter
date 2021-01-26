41e5b09e40534738cadd3666ddf05169e3c6464b
Harles <haryobimo8@gmail.com>
2021-01-26 23:00:01 +0700

    Initial commit

---
+++ src/assets/users.js
@@ -0,0 +1,40 @@
+export const users = [{
+        "id": 1,
+        "username": "Harles09",
+        "firstName": "Haryo",
+        "lastName": "Bimo",
+        "email": "haryobimo8@gmail.com",
+        "isAdmin": true,
+        "twoots": [
+            { "id": 1, "content": "Twotter is Amazing!" },
+            { "id": 2, "content": "Don't Forget follow my Twoot" }
+        ]
+    },
+    {
+        "id": 2,
+        "username": "Mrivaldo",
+        "firstName": "Dimas",
+        "lastName": "Noval",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 3,
+        "username": "Hamzah_",
+        "firstName": "M",
+        "lastName": "Hamzah",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 4,
+        "username": "Gifari_",
+        "firstName": "Giffari",
+        "lastName": "Fardiaz",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    }
+];
